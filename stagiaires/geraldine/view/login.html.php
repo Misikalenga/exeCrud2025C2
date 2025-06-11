dee6bffbabcbbccecd9c512aacb995c8c042044e
GEUNGA <ungaro.geraldine.gmail.com>
2025-06-11 11:39:30 +0200

    deuxième commit

--- stagiaires/geraldine/view/login.html.php
+++ stagiaires/geraldine/view/login.html.php
@@ -23,50 +23,6 @@
         <input type="submit" value="Se connecter">
     </form>
 
-    </main>
-<div class="container">
-    <div class="row">
 
-        <div class="col-lg-12">
-
-
-            <!-- Blog Posts Section -->
-            <section id="blog-posts" class="blog-posts section">
-
-                <div class="container">
-
-                    <div class="row gy-4">
-                        <div>
-                            <?php if(isset($error)): ?>
-                            <h3 class="alert alert-danger"><?=$error?></h3>
-                            <?php
-                            endif;
-                            ?>
-                            <form action="" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
-                                <div class="row gy-4">
-
-                                    <div class="col-md-6">
-                                        <input type="text" name="user_login" class="form-control" placeholder="votre login" required>
-                                    </div>
-
-                                    <div class="col-md-6 ">
-                                        <input type="password" class="form-control" name="user_pwd" placeholder="Votre mot de passe" required>
-                                    </div>
-
-                                    <div class="col-md-6 ">
-                                        <input type="submit" value="Se connecter">
-                                    </div>
-
-                                </div>
-                            </form>
-                        </div><!-- End Contact Form -->
-                        <div class="col-12">
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    </div>
-</div>
 </body>
 </html>
